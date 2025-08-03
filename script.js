e9a7ae8dabe31e46f35d741e7f9a3e1671f5efb1
Sumitkumbhar15 <sumitkumbhar204@gmail.com>
2025-08-03 15:03:59 +0530

    css and js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 let url = "https://catfact.ninja/fact";
+let btn = document.querySelector("button");
+
+btn.addEventListener("click" ,async () => {
+    let fact = await getCatFact();
+    let p = document.querySelector("#randomfact");
+    p.innerText = fact;
+})
+
+async function getCatFact() {
+    try {
+        let res = await axios.get(url);
+        return res.data.fact;
+    }
+    catch(e){
+        return "Oops! Could not fetch a cat fact.";
+        console.log(e);
+    }
+}
